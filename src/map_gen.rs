629b8fe6eaa77bd80f99e7ef53497be3fe63423f
Srinivas Kaza <srinivas@kaza.io>
2021-03-14 18:19:11 -0400

    Remove unneeded prefabs

--- src/map_gen.rs
+++ src/map_gen.rs
@@ -13,10 +13,7 @@ use crate::world::{Item, Mob, MobKind, Offset, Pos, Rect, TileKind, World, DIREC
 pub const SEGMENTS: &[(&[u8], (u32, u32))] = &[
     (include_bytes!("../static/0.png"), (10, 10)),
     (include_bytes!("../static/1.png"), (16, 16)), // make more stuff
-    (include_bytes!("../static/0.png"), (10, 10)),
-    (include_bytes!("../static/1.png"), (16, 16)),
     (include_bytes!("../static/cells.png"), (20, 20)),
-    (include_bytes!("../static/cells.png"), (40, 40)),
 ];
 
 macro_rules! avg {
