94434a5365b0872c29fd1f1d209c199db01384ca
Matthew Pfeiffer <spferical@gmail.com>
2021-03-13 23:22:27 -0500

    Rework bsp gen to expose necessarily adj rooms

--- src/map_gen.rs
+++ src/map_gen.rs
@@ -22,6 +22,7 @@ macro_rules! avg {
     };
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct CarveRoomOpts {
     wall: TileKind,
     floor: TileKind,
@@ -29,6 +30,38 @@ pub struct CarveRoomOpts {
     max_height: i32,
     min_width: i32,
     min_height: i32,
+}
+
+impl From<CarveRoomOpts> for BspSplitOpts {
+    fn from(opts: CarveRoomOpts) -> Self {
+        Self {
+            max_width: opts.max_width,
+            max_height: opts.max_height,
+            min_width: opts.min_width,
+            min_height: opts.min_height,
+        }
+    }
+}
+
+pub fn carve_rooms_bsp(
+    world: &mut World,
+    rect: Rect,
+    opts: &CarveRoomOpts,
+    rng: &mut impl Rng,
+) -> Vec<Rect> {
+    let tree = gen_bsp_tree(rect, (*opts).into(), rng);
+    let rooms_with_adj = tree.into_rooms_and_tree_adj();
+    for (room, adj_rooms) in &rooms_with_adj {
+        fill_rect(world, *room, opts.floor);
+        for adj in adj_rooms {
+            let wall = get_connecting_wall(*room, *adj).unwrap();
+            let has_door = wall.into_iter().any(|pos| world[pos].kind.is_walkable());
+            if !has_door {
+                carve_floor(world, wall.choose(rng), 0, opts.floor);
+            }
+        }
+    }
+    rooms_with_adj.into_iter().map(|r| r.0).collect()
 }
 
 pub fn carve_rooms_bsp_extra_loops(
@@ -86,12 +119,60 @@ fn get_connecting_wall(room1: Rect, room2: Rect) -> Option<Rect> {
     None
 }
 
-pub fn carve_rooms_bsp(
-    world: &mut World,
-    rect: Rect,
-    opts: &CarveRoomOpts,
-    rng: &mut impl Rng,
-) -> Vec<Rect> {
+// given rect, get rooms this room can connect to
+// returns vec of (connecting walls, other room)
+fn get_adjacent(room: Rect, rooms: &Vec<Rect>) -> Vec<(Rect, Rect)> {
+    let mut adj = Vec::new();
+    for other in rooms.iter().cloned() {
+        if let Some(wall) = get_connecting_wall(room, other) {
+            adj.push((wall, other));
+        }
+    }
+    adj
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct BspSplitOpts {
+    max_width: i32,
+    max_height: i32,
+    min_width: i32,
+    min_height: i32,
+}
+
+pub enum BspTree {
+    Split(Box<BspTree>, Box<BspTree>),
+    Room(Rect),
+}
+
+impl BspTree {
+    fn into_rooms_and_tree_adj(self) -> Vec<(Rect, Vec<Rect>)> {
+        match self {
+            BspTree::Room(rect) => vec![(rect, vec![])],
+            BspTree::Split(tree1, tree2) => {
+                let mut rooms1 = tree1.into_rooms_and_tree_adj();
+                let mut rooms2 = tree2.into_rooms_and_tree_adj();
+                // now figure out how to bridge the trees
+                let mut bridged = false;
+                'loop1: for (room1, ref mut adj1) in &mut rooms1 {
+                    for (room2, ref mut adj2) in &mut rooms2 {
+                        if get_connecting_wall(*room1, *room2).is_some() {
+                            adj1.push(*room2);
+                            adj2.push(*room1);
+                            bridged = true;
+                            break 'loop1;
+                        }
+                    }
+                }
+                assert!(bridged);
+                rooms1.extend(rooms2);
+                rooms1
+            }
+        }
+    }
+}
+
+// returns (rooms, walls between connected rooms in the bsp tree)
+pub fn gen_bsp_tree(rect: Rect, opts: BspSplitOpts, rng: &mut impl Rng) -> BspTree {
     assert!(opts.min_width * 2 + 1 < opts.max_width);
     assert!(opts.min_height * 2 + 1 < opts.max_height);
     #[derive(Clone, Copy, Debug)]
@@ -112,63 +193,23 @@ pub fn carve_rooms_bsp(
         Split::X => {
             let split_x =
                 rng.gen_range(rect.x1 + opts.min_width + 1..(rect.x2 - opts.min_width - 1));
-            let mut rooms = carve_rooms_bsp(
-                world,
-                Rect::new(rect.x1, split_x - 1, rect.y1, rect.y2),
-                opts,
-                rng,
-            );
-            rooms.extend(carve_rooms_bsp(
-                world,
-                Rect::new(split_x + 1, rect.x2, rect.y1, rect.y2),
-                opts,
-                rng,
-            ));
-            let mid_left = Pos {
-                x: avg!(rect.x1, split_x - 1),
-                y: avg!(rect.y1, rect.y2),
-            };
-            let mid_right = Pos {
-                x: avg!(split_x + 1, rect.x2),
-                y: avg!(rect.y1, rect.y2),
-            };
-            carve_line(world, mid_left, mid_right, 0, opts.floor);
-            rooms
+            let left = Rect::new(rect.x1, split_x - 1, rect.y1, rect.y2);
+            let right = Rect::new(split_x + 1, rect.x2, rect.y1, rect.y2);
+            BspTree::Split(
+                Box::new(gen_bsp_tree(left, opts, rng)),
+                Box::new(gen_bsp_tree(right, opts, rng)),
+            )
         }
         Split::Y => {
             let split_y = rng.gen_range(rect.y1 + opts.min_height + 1..(rect.y2 - opts.min_height));
-            let mut rooms = carve_rooms_bsp(
-                world,
-                Rect::new(rect.x1, rect.x2, rect.y1, split_y - 1),
-                opts,
-                rng,
-            );
-            rooms.extend(carve_rooms_bsp(
-                world,
-                Rect::new(rect.x1, rect.x2, split_y + 1, rect.y2),
-                opts,
-                rng,
-            ));
-            let mid_top = Pos {
-                x: avg!(rect.x1, rect.x2),
-                y: avg!(rect.y1, split_y - 1),
-            };
-            let mid_bot = Pos {
-                x: avg!(rect.x1, rect.x2),
-                y: avg!(split_y + 1, rect.y2),
-            };
-            carve_line(world, mid_top, mid_bot, 0, opts.floor);
-            rooms
+            let top = Rect::new(rect.x1, rect.x2, rect.y1, split_y - 1);
+            let bottom = Rect::new(rect.x1, rect.x2, split_y + 1, rect.y2);
+            BspTree::Split(
+                Box::new(gen_bsp_tree(top, opts, rng)),
+                Box::new(gen_bsp_tree(bottom, opts, rng)),
+            )
         }
-        Split::None => {
-            // just carve the room
-            fill_rect(
-                world,
-                Rect::new(rect.x1, rect.x2, rect.y1, rect.y2),
-                opts.floor,
-            );
-            vec![rect]
-        }
+        Split::None => BspTree::Room(rect),
     }
 }
 
@@ -450,6 +491,7 @@ pub fn generate_world(world: &mut World, seed: u64) {
         TileKind::BloodyFloor,
     );
     carve_floor(world, edge + Offset { x: 2, y: 0 }, 0, TileKind::Computer);
+    //gen_lab_complex(world, &mut rng, Pos::new(8, 0), Rect::new(8, 50, -25, 25));
 }
 
 pub fn carve_floor(world: &mut World, pos: Pos, brush_size: u8, tile: TileKind) {
@@ -463,21 +505,25 @@ pub fn carve_floor(world: &mut World, pos: Pos, brush_size: u8, tile: TileKind) 
     }
 }
 
-// my fancy new map gen stuff!
 pub fn gen_lab_complex(world: &mut World, rng: &mut impl Rng, left_entrance: Pos, rect: Rect) {
     fill_rect(world, rect, TileKind::Wall);
     carve_floor(world, left_entrance, 1, TileKind::Floor);
-    let bsp_opts = CarveRoomOpts {
-        wall: TileKind::Wall,
-        floor: TileKind::Wall,
+    let bsp_opts = BspSplitOpts {
         max_width: 30,
         max_height: 30,
         min_width: 5,
         min_height: 5,
     };
 
-    let rect = Rect::new(rect.x1 + 1, rect.x2 - 1, rect.y1 + 1, rect.y2 - 1);
-    let segments = carve_rooms_bsp(world, rect, &bsp_opts, rng);
+    let tree = gen_bsp_tree(rect, bsp_opts, rng);
+    let rooms_with_adj = tree.into_rooms_and_tree_adj();
+
+    // todo: add non-tree connections
+    // maybe add them as optional connections for the segment carver
+
+    for (room, adj_rooms) in rooms_with_adj {
+        carve_segment(world, 0, room, TileKind::Floor, rng);
+    }
 }
 
 pub fn carve_segment(
